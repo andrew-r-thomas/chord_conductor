d4e62ecdaf1ee891bd62d68412e2bc72b8b4080d
Andrew Thomas <andrew@andrewrthomas.com>
2024-09-09 20:05:46 -0700

    ok something

--- src/node.rs
+++ src/node.rs
@@ -1,7 +1,15 @@
-use node::{node_service_server::NodeService, SetRequest, SetResponse};
+use node::{
+    node_service_client::NodeServiceClient,
+    node_service_server::{NodeService, NodeServiceServer},
+    SetRequest, SetResponse,
+};
+use tokio::sync::mpsc::{self, Sender};
 
-use std::{collections::HashMap, sync::Arc};
-use tonic::{transport::Server, Request, Response, Status};
+use std::collections::HashMap;
+use tonic::{
+    transport::{Channel, Server},
+    Request, Response, Status,
+};
 
 use sha2::{Digest, Sha256};
 
@@ -9,46 +17,99 @@ pub mod node {
     tonic::include_proto!("node");
 }
 
+#[derive(Debug)]
 pub struct Node {
     id: String,
-    data: HashMap<String, String>,
-    sucessor: Option<String>,
-    predecessor: Option<String>,
+    sucessor: String,
+    predecessor: String,
+    sender: Sender<Command>,
+}
+
+pub enum Location {
+    Local,
+    Remote,
+}
+pub enum Command {
+    Set {
+        key: String,
+        val: String,
+        location: Location,
+    },
 }
 
 #[tonic::async_trait]
 impl NodeService for Node {
     async fn set(&self, request: Request<SetRequest>) -> Result<Response<SetResponse>, Status> {
-        // NOTE: good job getting back to it, do this piece next
+        let request_message = request.into_inner();
+        let mut hasher = Sha256::new();
+
+        Digest::update(&mut hasher, request_message.key.as_bytes());
+        let hash = String::from_utf8(hasher.finalize().to_vec()).unwrap();
+        let command = match hash <= self.id && hash >= self.predecessor {
+            true => Command::Set {
+                key: request_message.key,
+                val: request_message.val,
+                location: Location::Local,
+            },
+            false => Command::Set {
+                key: request_message.key,
+                val: request_message.val,
+                location: Location::Remote,
+            },
+        };
+
+        self.sender.send(command).await.unwrap();
+
+        Ok(Response::new(SetResponse {}))
     }
 }
 
 impl Node {
-    pub fn start(id: String) -> Self {
-        let data = HashMap::<String, String>::new();
-
+    pub fn new(id: String, sender: Sender<Command>) -> Self {
         Self {
+            sucessor: id.clone(),
+            predecessor: id.clone(),
             id,
-            data,
-            sucessor: None,
-            predecessor: None,
+            sender,
         }
     }
-    pub fn get() {}
-    pub fn set(&mut self, key: String, val: String, nodes: Arc<HashMap<String, Self>>) {
-        let mut hasher = Sha256::new();
-        hasher.update(key.as_bytes());
-        // PERF: i dont like creating a vec every time
-        // TODO: unwrap
-        // TODO: figure out if strings are really what we want here
-        let hash = String::from_utf8(hasher.finalize().to_vec()).unwrap();
-        // TODO: unwrap
-        if hash <= self.id && &hash >= self.predecessor.as_ref().unwrap() {
-            self.data.insert(key, val);
-        } else {
-            // TODO: unwrap
-            let sucessor = nodes.get_mut(self.sucessor.as_ref().unwrap()).unwrap();
-            sucessor.set(key, val, nodes.clone());
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (sender, mut reciever) = mpsc::channel::<Command>(32);
+    // TODO: figure out ports
+    let addr = "[::1]:50051".parse()?;
+    let node = Node::new("this is a string".into(), sender.clone());
+
+    Server::builder()
+        .add_service(NodeServiceServer::new(node))
+        .serve(addr)
+        .await?;
+
+    let manager = tokio::spawn(async move {
+        let mut node_client: NodeServiceClient<Channel> =
+            NodeServiceClient::connect("http://[::1]:50051")
+                .await
+                .unwrap();
+        let mut data = HashMap::<String, String>::new();
+
+        while let Some(command) = reciever.recv().await {
+            match command {
+                Command::Set { key, val, location } => match location {
+                    Location::Local => {
+                        data.insert(key, val);
+                    }
+                    Location::Remote => {
+                        let request = tonic::Request::new(SetRequest { key, val });
+                        node_client.set(request).await.unwrap();
+                    }
+                },
+            }
         }
-    }
+    });
+
+    manager.await.unwrap();
+
+    Ok(())
 }
