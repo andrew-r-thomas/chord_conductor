f91684da2069befeb37a087f820b7f608ffae8fc
Andrew Thomas <andrew@andrewrthomas.com>
2024-09-12 14:00:46 -0700

    getting somewhere with the visuals

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,185 @@
-use tokio::{
-    process::Command,
-    time::{sleep, Duration},
+use core::panic;
+use std::{
+    collections::BTreeMap,
+    sync::{Arc, Mutex},
 };
-use tonic::Request;
+
+use axum::{
+    extract::{
+        ws::{Message, WebSocket},
+        ConnectInfo, WebSocketUpgrade,
+    },
+    response::IntoResponse,
+    routing::get,
+    serve, Router, ServiceExt,
+};
+use axum_extra::TypedHeader;
+use futures::{
+    channel::oneshot,
+    sink::SinkExt,
+    stream::{SplitSink, StreamExt},
+};
+use serde::{Deserialize, Serialize};
+use tokio::{
+    net::TcpListener,
+    process::Command,
+    task::JoinSet,
+    time::{self, interval, sleep, Duration, Instant, Interval},
+};
+use tonic::{transport::Channel, Request};
+
 mod node {
     tonic::include_proto!("node");
 }
-
-use node::{get_request::Getter, node_service_client::NodeServiceClient, GetRequest, SetRequest};
+use node::{
+    get_request::Getter, node_service_client::NodeServiceClient, GetRequest, GetStateRequest,
+    SetRequest,
+};
 
 #[tokio::main]
 async fn main() {
-    let _node_child_process_1 = Command::new("./target/debug/node")
-        .kill_on_drop(true)
-        .args(&["0.0.0.0:50051"])
-        .spawn()
-        .expect("failed to start node");
-    // TODO: maybe a mechanism for knowing when a node is ready
-    sleep(Duration::from_millis(1000)).await;
+    // let _node_child_process_1 = Command::new("./target/debug/node")
+    //     .kill_on_drop(true)
+    //     .args(&["0.0.0.0:50051"])
+    //     .spawn()
+    //     .expect("failed to start node");
+    // // TODO: maybe a mechanism for knowing when a node is ready
+    // sleep(Duration::from_millis(1000)).await;
+    //
+    // let _node_child_process_2 = Command::new("./target/debug/node")
+    //     .kill_on_drop(true)
+    //     .args(&["0.0.0.0:50052", "0.0.0.0:50051"])
+    //     .spawn()
+    //     .expect("failed to start node");
+    // sleep(Duration::from_millis(1000)).await;
+    //
+    // let mut client = NodeServiceClient::connect("http://0.0.0.0:50051")
+    //     .await
+    //     .unwrap();
+    //
+    // let key = "this is a test";
+    // let val = "these are some bytes";
+    // let request = Request::new(SetRequest {
+    //     key: key.into(),
+    //     val: val.into(),
+    // });
+    //
+    // let set_resp = client.set(request).await.unwrap();
+    // let set_data = set_resp.into_inner();
+    // println!("key: {}, val: {} inserted on: {}", key, val, set_data.loc);
+    //
+    // let get_request = Request::new(GetRequest {
+    //     getter: Some(Getter::Key(key.into())),
+    // });
+    //
+    // let get_resp = client.get(get_request).await.unwrap();
+    // let get_data = get_resp.into_inner();
+    // println!(
+    //     "value of key: {} is: {}, located on: {}",
+    //     key, get_data.val, get_data.loc
+    // );
 
-    let _node_child_process_2 = Command::new("./target/debug/node")
-        .kill_on_drop(true)
-        .args(&["0.0.0.0:50052", "0.0.0.0:50051"])
-        .spawn()
-        .expect("failed to start node");
-    sleep(Duration::from_millis(1000)).await;
+    let app = Router::new().route("/ws", get(ws_handler));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    serve(listener, app).await.unwrap();
+}
 
-    let mut client = NodeServiceClient::connect("http://0.0.0.0:50051")
-        .await
-        .unwrap();
+async fn ws_handler(
+    ws: WebSocketUpgrade,
+    user_agent: Option<TypedHeader<headers::UserAgent>>,
+) -> impl IntoResponse {
+    let user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
+        user_agent.to_string()
+    } else {
+        String::from("Unknown browser")
+    };
 
-    let key = "this is a test";
-    let val = "these are some bytes";
-    let request = Request::new(SetRequest {
-        key: key.into(),
-        val: val.into(),
+    println!("`{user_agent}` connected.");
+
+    ws.on_upgrade(move |socket| handle_socket(socket))
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum WsTextMessage {
+    Start,
+    Other,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct NodeState {
+    addr: String,
+    pred: String,
+    succ: String,
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    socket.send(Message::Ping(vec![])).await.unwrap();
+    socket.recv().await.unwrap().unwrap();
+
+    let (mut send, mut recv) = socket.split();
+
+    let (poll_start_send, poll_start_recv) = oneshot::channel::<()>();
+    let poll_task = tokio::spawn(async move {
+        poll_start_recv.await.unwrap();
+
+        let mut clients = BTreeMap::<String, NodeServiceClient<Channel>>::new();
+
+        let node_addr = "0.0.0.0:50051";
+        let _node = Command::new("./target/debug/node")
+            .kill_on_drop(true)
+            .args(&[node_addr])
+            .spawn()
+            .expect("failed to start node");
+        // TODO: maybe a mechanism for knowing when a node is ready
+        sleep(Duration::from_millis(1000)).await;
+        let client = NodeServiceClient::connect(format!("http://{}", node_addr))
+            .await
+            .unwrap();
+
+        clients.insert(node_addr.into(), client);
+
+        let mut interval = interval(Duration::from_millis(500));
+        loop {
+            interval.tick().await;
+
+            // PERF: make this parallel
+            for client_conn in clients.iter_mut() {
+                let get_state_resp = client_conn
+                    .1
+                    .get_state(Request::new(GetStateRequest {}))
+                    .await
+                    .unwrap();
+                let get_state_message = get_state_resp.into_inner();
+                send.send(Message::Text(
+                    serde_json::to_string(&NodeState {
+                        addr: client_conn.0.clone(),
+                        pred: get_state_message.pred,
+                        succ: get_state_message.succ,
+                    })
+                    .unwrap(),
+                ))
+                .await
+                .unwrap();
+            }
+        }
     });
 
-    let set_resp = client.set(request).await.unwrap();
-    let set_data = set_resp.into_inner();
-    println!("key: {}, val: {} inserted on: {}", key, val, set_data.loc);
-
-    let get_request = Request::new(GetRequest {
-        getter: Some(Getter::Key(key.into())),
+    let recv_task = tokio::spawn(async move {
+        match recv.next().await {
+            Some(Ok(Message::Text(msg_text))) => {
+                let ws_text_message: WsTextMessage = serde_json::from_str(&msg_text).unwrap();
+                match ws_text_message {
+                    WsTextMessage::Start => {
+                        println!("start called");
+                        poll_start_send.send(()).unwrap();
+                    }
+                    _ => panic!(),
+                }
+            }
+            _ => panic!(),
+        }
     });
 
-    let get_resp = client.get(get_request).await.unwrap();
-    let get_data = get_resp.into_inner();
-    println!(
-        "value of key: {} is: {}, located on: {}",
-        key, get_data.val, get_data.loc
-    );
+    recv_task.await.unwrap();
+    poll_task.await.unwrap();
 }
--- src/node/main.rs
+++ src/node/main.rs
@@ -24,8 +24,9 @@ use node_service::{
     node_service_client::NodeServiceClient,
     node_service_server::{NodeService, NodeServiceServer},
     notify_request::HasData,
-    notify_response, Data, GetRequest, GetResponse, JoinRequest, JoinResponse, NotifyRequest,
-    NotifyResponse, PredRequest, PredResponse, SetRequest, SetResponse,
+    notify_response, Data, GetRequest, GetResponse, GetStateRequest, GetStateResponse, JoinRequest,
+    JoinResponse, NotifyRequest, NotifyResponse, PredRequest, PredResponse, SetRequest,
+    SetResponse,
 };
 
 #[derive(Debug)]
@@ -34,8 +35,20 @@ pub struct Node {
     id: Vec<u8>,
     data: Arc<RwLock<BTreeMap<Vec<u8>, String>>>,
     // TODO: whenever theres a change to pred or succ, we need to do data splits
-    pred: Arc<RwLock<(Vec<u8>, String)>>,
-    succ: Arc<RwLock<VecDeque<(Vec<u8>, NodeServiceClient<Channel>)>>>,
+    pred: Arc<RwLock<PredHandle>>,
+    succ: Arc<RwLock<VecDeque<SuccHandle>>>,
+}
+
+#[derive(Debug)]
+pub struct SuccHandle {
+    addr: String,
+    hash: Vec<u8>,
+    client: NodeServiceClient<Channel>,
+}
+#[derive(Debug)]
+pub struct PredHandle {
+    addr: String,
+    hash: Vec<u8>,
 }
 
 #[tonic::async_trait]
@@ -49,7 +62,10 @@ impl NodeService for Node {
         hasher.update(request_message.key.as_bytes());
         let hash = hasher.finalize().to_vec();
 
-        match self.within_range(&hash).await || self.succ.read().await.is_empty() {
+        let pred_read = self.pred.read().await;
+        match pred_read.addr.is_empty()
+            || Self::within_range(&hash, pred_read.hash.as_slice(), self.id.as_slice())
+        {
             true => {
                 self.data.write().await.insert(hash, request_message.val);
                 Ok(Response::new(SetResponse {
@@ -60,7 +76,7 @@ impl NodeService for Node {
             false => {
                 let mut succ_lock = self.succ.write().await;
                 let first_succ = succ_lock.front_mut().unwrap();
-                let succ_write = &mut first_succ.1;
+                let succ_write = &mut first_succ.client;
 
                 succ_write
                     .set(Request::new(SetRequest {
@@ -83,7 +99,10 @@ impl NodeService for Node {
             }
             Getter::Hash(hash) => hash,
         };
-        match self.within_range(&hash).await || self.succ.read().await.is_empty() {
+        let pred_read = self.pred.read().await;
+        match pred_read.addr.is_empty()
+            || Self::within_range(&hash, pred_read.hash.as_slice(), self.id.as_slice())
+        {
             true => {
                 let reader = self.data.read().await;
                 let val = reader.get(&hash).unwrap();
@@ -96,7 +115,7 @@ impl NodeService for Node {
             false => {
                 let mut succ_lock = self.succ.write().await;
                 let first_succ = succ_lock.front_mut().unwrap();
-                let succ_write = &mut first_succ.1;
+                let succ_write = &mut first_succ.client;
 
                 succ_write.get(Request::new(request_message)).await
             }
@@ -110,10 +129,18 @@ impl NodeService for Node {
         hasher.update(request_message.ip.as_bytes());
         let hash = hasher.finalize().to_vec();
 
-        match self.within_range(&hash).await || self.succ.read().await.is_empty() {
+        let is_pred = {
+            let pred_read = self.pred.read().await;
+            pred_read.addr.is_empty()
+                || Self::within_range(&hash, pred_read.hash.as_slice(), self.id.as_slice())
+        };
+        match is_pred {
             true => {
-                let (keys, vals) = self.shed_data(&hash).await;
-                *self.pred.write().await = (hash, request_message.ip);
+                let mut data_write = self.data.write().await;
+                let mut pred_write = self.pred.write().await;
+                let (keys, vals) = Self::shed_data(&mut data_write, &hash, &self.id, true);
+                pred_write.hash = hash;
+                pred_write.addr = request_message.ip;
 
                 Ok(Response::new(JoinResponse {
                     succ_ip: self.addr.clone(),
@@ -125,7 +152,7 @@ impl NodeService for Node {
             false => {
                 let mut succ_lock = self.succ.write().await;
                 let first_succ = succ_lock.front_mut().unwrap();
-                let succ_write = &mut first_succ.1;
+                let succ_write = &mut first_succ.client;
 
                 succ_write.join(Request::new(request_message)).await
             }
@@ -133,10 +160,11 @@ impl NodeService for Node {
     }
 
     async fn pred(&self, _request: Request<PredRequest>) -> Result<Response<PredResponse>, Status> {
+        // TODO: need to make sure we're not sending empty preds here
         let pred_read = self.pred.read().await;
         Ok(Response::new(PredResponse {
-            addr: pred_read.1.clone(),
-            hash: pred_read.0.clone(),
+            addr: pred_read.addr.clone(),
+            hash: pred_read.hash.clone(),
         }))
     }
 
@@ -147,7 +175,12 @@ impl NodeService for Node {
         let request_message = request.into_inner();
         let is_pred = {
             let pred_read = self.pred.read().await;
-            pred_read.0.is_empty() || self.within_range(&request_message.hash).await
+            pred_read.addr.is_empty()
+                || Self::within_range(
+                    request_message.hash.as_slice(),
+                    pred_read.hash.as_slice(),
+                    self.id.as_slice(),
+                )
         };
         let response_message = match is_pred {
             true => {
@@ -155,7 +188,6 @@ impl NodeService for Node {
                 let mut data_write = self.data.write().await;
                 let out = match request_message.hash > self.id {
                     true => {
-                        let first_chunk = data_write.split_off(key)
                         todo!()
                     }
                     false => {
@@ -171,8 +203,8 @@ impl NodeService for Node {
                         }
                     }
                 };
-                pred_write.0 = request_message.hash;
-                pred_write.1 = request_message.addr;
+                pred_write.hash = request_message.hash;
+                pred_write.addr = request_message.addr;
                 out
             }
             false => NotifyResponse { has_data: None },
@@ -180,12 +212,28 @@ impl NodeService for Node {
 
         Ok(Response::new(response_message))
     }
+
+    async fn get_state(
+        &self,
+        _request: Request<GetStateRequest>,
+    ) -> Result<Response<GetStateResponse>, Status> {
+        let succ_read = self.succ.read().await;
+        let pred_read = self.pred.read().await;
+        let succ = match succ_read.front() {
+            Some(s) => s.addr.clone(),
+            None => String::from("None"),
+        };
+        Ok(Response::new(GetStateResponse {
+            succ,
+            pred: pred_read.addr.clone(),
+        }))
+    }
 }
 
 impl Node {
     pub fn new(
         addr: String,
-        succ: Arc<RwLock<VecDeque<(Vec<u8>, NodeServiceClient<Channel>)>>>,
+        succ: Arc<RwLock<VecDeque<SuccHandle>>>,
         data: Arc<RwLock<BTreeMap<Vec<u8>, String>>>,
     ) -> Self {
         let mut hasher = Sha256::new();
@@ -193,36 +241,15 @@ impl Node {
         let id = hasher.finalize().to_vec();
 
         Self {
-            pred: Arc::new(RwLock::new((vec![], String::new()))),
+            pred: Arc::new(RwLock::new(PredHandle {
+                addr: String::new(),
+                hash: vec![],
+            })),
             succ,
             id,
             addr,
             data,
         }
-    }
-
-    pub async fn within_range(&self, key: &[u8]) -> bool {
-        // PERF: there is only one node that crosses this boundary,
-        // and we will be doing this check a lot
-        let p_read = self.pred.read().await;
-        let p = p_read.0.as_slice();
-        if p >= self.id.as_slice() {
-            key > p || key <= self.id.as_slice()
-        } else {
-            key <= self.id.as_slice() && key > p
-        }
-    }
-
-    pub async fn shed_data(&self, pred_hash: &[u8]) -> (Vec<Vec<u8>>, Vec<String>) {
-        let mut data = self.data.write().await;
-        let to_keep = data.split_off(pred_hash);
-        let to_shed = data.clone();
-
-        *data = to_keep;
-        (
-            to_shed.keys().cloned().collect_vec(),
-            to_shed.values().cloned().collect_vec(),
-        )
     }
 
     pub fn start_periodics(&self, stabilize_interval: Duration) -> JoinHandle<()> {
@@ -239,19 +266,16 @@ impl Node {
                 interval.tick().await;
                 let mut succ_lock = succ.write().await;
                 if let Some(first_succ) = succ_lock.front_mut() {
-                    let succ_write = &mut first_succ.1;
+                    let succ_write = &mut first_succ.client;
                     let pred = succ_write.pred(Request::new(PredRequest {})).await.unwrap();
                     let pred_data = pred.into_inner();
 
                     // check if its between this node and the successor
-                    let in_range = {
-                        let succ_hash = &first_succ.0;
-                        if succ_hash < &id {
-                            &pred_data.hash < succ_hash || &pred_data.hash > &id
-                        } else {
-                            &pred_data.hash > &id && &pred_data.hash < succ_hash
-                        }
-                    };
+                    let in_range = Node::within_range(
+                        pred_data.hash.as_slice(),
+                        first_succ.hash.as_slice(),
+                        id.as_slice(),
+                    );
 
                     let notify_message = match in_range {
                         true => {
@@ -264,7 +288,11 @@ impl Node {
                             // FIXME: we need to fix splif off calls to handle
                             // crossing 0 boundary
                             let to_send = data_write.split_off(&pred_data.hash);
-                            succ_lock.push_front((pred_data.hash, new_succ));
+                            succ_lock.push_front(SuccHandle {
+                                addr: pred_data.addr,
+                                hash: pred_data.hash,
+                                client: new_succ,
+                            });
 
                             NotifyRequest {
                                 addr: addr.clone(),
@@ -283,7 +311,7 @@ impl Node {
                     };
 
                     let current_succ = succ_lock.front_mut().unwrap();
-                    let succ_client = &mut current_succ.1;
+                    let succ_client = &mut current_succ.client;
                     let notify_response = succ_client
                         .notify(Request::new(notify_message))
                         .await
@@ -301,15 +329,15 @@ impl Node {
                     }
                 } else {
                     let pred_read = pred.read().await;
-                    if !pred_read.0.is_empty() {
+                    if !pred_read.addr.is_empty() {
                         let mut new_succ =
-                            NodeServiceClient::connect(format!("http://{}", pred_read.1))
+                            NodeServiceClient::connect(format!("http://{}", pred_read.addr))
                                 .await
                                 .unwrap();
 
                         let mut data_write = data.write().await;
                         let first_chunk = data_write.split_off(&id);
-                        let keeping = data_write.split_off(&pred_read.0);
+                        let keeping = data_write.split_off(&pred_read.hash);
                         let second_chunk = data_write.clone();
                         *data_write = keeping;
 
@@ -344,11 +372,77 @@ impl Node {
                             }
                         }
 
-                        succ_lock.push_front((pred_read.0.clone(), new_succ));
+                        succ_lock.push_front(SuccHandle {
+                            addr: pred_read.addr.clone(),
+                            hash: pred_read.hash.clone(),
+                            client: new_succ,
+                        });
                     }
                 }
             }
         })
+    }
+
+    pub fn within_range(target: &[u8], start: &[u8], end: &[u8]) -> bool {
+        match start > end {
+            true => target > start || target < end,
+            false => target > start && target < end,
+        }
+    }
+
+    pub fn shed_data(
+        data: &mut BTreeMap<Vec<u8>, String>,
+        split_start: &[u8],
+        split_end: &[u8],
+        keep_split: bool,
+    ) -> (Vec<Vec<u8>>, Vec<String>) {
+        match (keep_split, split_start > split_end) {
+            (true, true) => {
+                let mut first_keep = data.split_off(split_start);
+                let to_send = data.split_off(split_end);
+
+                data.append(&mut first_keep);
+
+                (
+                    to_send.clone().into_keys().collect_vec(),
+                    to_send.clone().into_values().collect_vec(),
+                )
+            }
+            (true, false) => {
+                let mut to_send = data.split_off(split_end);
+                let to_keep = data.split_off(split_start);
+
+                to_send.append(data);
+                *data = to_keep;
+
+                (
+                    to_send.clone().into_keys().collect_vec(),
+                    to_send.clone().into_values().collect_vec(),
+                )
+            }
+            (false, true) => {
+                let mut to_send = data.split_off(split_start);
+                let to_keep = data.split_off(split_end);
+
+                to_send.append(data);
+                *data = to_keep;
+
+                (
+                    to_send.clone().into_keys().collect_vec(),
+                    to_send.clone().into_values().collect_vec(),
+                )
+            }
+            (false, false) => {
+                let mut to_keep = data.split_off(split_end);
+                let to_send = data.split_off(split_start);
+                data.append(&mut to_keep);
+
+                (
+                    to_send.clone().into_keys().collect_vec(),
+                    to_send.clone().into_values().collect_vec(),
+                )
+            }
+        }
     }
 }
 
@@ -377,23 +471,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .zip(join_resp_data.vals.into_iter()),
             )));
 
-            let succ = Arc::new(RwLock::new(
-                VecDeque::<(Vec<u8>, NodeServiceClient<Channel>)>::from_iter(
-                    &mut [(
-                        join_resp_data.succ_id,
-                        NodeServiceClient::connect(format!("http://{}", join_resp_data.succ_ip))
-                            .await?,
-                    )]
-                    .into_iter(),
-                ),
-            ));
+            let succ = Arc::new(RwLock::new(VecDeque::<SuccHandle>::from_iter(
+                &mut [SuccHandle {
+                    addr: join_resp_data.succ_ip.clone(),
+                    hash: join_resp_data.succ_id,
+                    client: NodeServiceClient::connect(format!(
+                        "http://{}",
+                        join_resp_data.succ_ip
+                    ))
+                    .await?,
+                }]
+                .into_iter(),
+            )));
             (data, succ)
         }
         None => {
             let data = Arc::new(RwLock::new(BTreeMap::<Vec<u8>, String>::new()));
-            let succ = Arc::new(RwLock::new(
-                VecDeque::<(Vec<u8>, NodeServiceClient<Channel>)>::new(),
-            ));
+            let succ = Arc::new(RwLock::new(VecDeque::<SuccHandle>::new()));
             (data, succ)
         }
     };
